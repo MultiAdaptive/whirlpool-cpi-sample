6f53365ffd202a12accdcb1b471a9adc6d9f9bf5
Winter <huangdongdong910@163.com>
2024-12-20 21:00:00 +0800

    ops: append accs

---
+++ tests/test-validator/accounts/jlp_sol_accs_6a3m2EgFFKfsFuQtP4LJJXPcAe3TQYXNyHUjjZpUxYgd/downacc.sh
@@ -0,0 +1,6 @@
+solana account -u m Df6Xh8vAyZJtnKLfShptMWHcwMLwzFAiqxupRTbBnpaV --output-file Df6Xh8vAyZJtnKLfShptMWHcwMLwzFAiqxupRTbBnpaV.json --output json-compact
+solana account -u m A8kEy5wWgdW4FG593fQJ5QPVbqx1wkfXw9c4L9bPo2CN --output-file A8kEy5wWgdW4FG593fQJ5QPVbqx1wkfXw9c4L9bPo2CN.json --output json-compact
+solana account -u m D8cy77BBepLMngZx6ZukaTff5hCt1HrWyKk3Hnd9oitf --output-file D8cy77BBepLMngZx6ZukaTff5hCt1HrWyKk3Hnd9oitf.json --output json-compact
+solana account -u m 8jNT2Qtyv2kFgNTo5S3htpXfQw3dYnTmnE5qx5enmFXV --output-file 8jNT2Qtyv2kFgNTo5S3htpXfQw3dYnTmnE5qx5enmFXV.json --output json-compact
+solana account -u m EZWQXxXAShgUKJjn7qRZe4dn3htj12mzsw39Nf4wzbKS --output-file EZWQXxXAShgUKJjn7qRZe4dn3htj12mzsw39Nf4wzbKS.json --output json-compact
+solana account -u m 4zKPdJqfhFW9FRPbUd3iuZmX4jHi2Lwsqbvh86B5AYEK --output-file 4zKPdJqfhFW9FRPbUd3iuZmX4jHi2Lwsqbvh86B5AYEK.json --output json-compact
